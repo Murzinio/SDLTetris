786b74370ec9a17faf42b7721182ac272e85ff56
Murzinio <k.pawelczykbsh@gmail.com>
2017-04-07 22:08:02 +0200

    Adjusted button scaling.

--- Tetris/headers/constants.h
+++ Tetris/headers/constants.h
@@ -1,7 +1,7 @@
 #pragma once
 
-const int SCREEN_HEIGHT = 540;
-const int SCREEN_WIDTH = 960;
+const int SCREEN_WIDTH = 192;
+const int SCREEN_HEIGHT = 108;
 
 //const int BUTTON_HEIGHT = 128;
 //const int BUTTON_WIDTH = 512;
--- Tetris/source/VEScaledDimensions.cpp
+++ Tetris/source/VEScaledDimensions.cpp
@@ -4,6 +4,6 @@ int VEScaledDimensions::GLOBAL_gameBoardWidth = SCREEN_WIDTH - SCREEN_WIDTH / 5;
 int VEScaledDimensions::GLOBAL_gameBoardHeight = SCREEN_HEIGHT - SCREEN_HEIGHT / 10;
 
 int VEScaledDimensions::GLOBAL_buttonWidth = SCREEN_WIDTH / 4;
-int VEScaledDimensions::GLOBAL_buttonHeight = SCREEN_WIDTH / 9;
+int VEScaledDimensions::GLOBAL_buttonHeight = SCREEN_WIDTH / 12;
 
 int VEScaledDimensions::GLOBAL_tetrominoBlockSize = SCREEN_WIDTH / 60;;
